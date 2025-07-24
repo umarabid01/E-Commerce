3e28ca1efa5252f24a240d52f44191f7646706c2
tola <tola@gmail.com>
2025-07-24 18:01:46 +0500

    new

--- backend/server.js
+++ backend/server.js
@@ -272,6 +272,9 @@ app.post("/api/billing", async (req, res) => {
   }
 })
 
+app.get("/", (req, res) => {
+  res.send("Backend API is running!");
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
